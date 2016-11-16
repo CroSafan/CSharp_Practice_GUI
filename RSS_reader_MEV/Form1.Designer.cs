4e89882f580cd3f52794ff068e3e6d5da5ace7a3
Antun Safaric <crosafan@gmail.com>
2016-11-16 23:01:07 +0100

    Enabled reading from a rss feed

--- RSS_reader_MEV/Form1.Designer.cs
+++ RSS_reader_MEV/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.titlesComboBox = new System.Windows.Forms.ComboBox();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(148, 33);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(149, 23);
+            this.refreshButton.TabIndex = 0;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // titlesComboBox
+            // 
+            this.titlesComboBox.FormattingEnabled = true;
+            this.titlesComboBox.Location = new System.Drawing.Point(38, 72);
+            this.titlesComboBox.Name = "titlesComboBox";
+            this.titlesComboBox.Size = new System.Drawing.Size(259, 21);
+            this.titlesComboBox.TabIndex = 1;
+            this.titlesComboBox.SelectedIndexChanged += new System.EventHandler(this.titlesComboBox_SelectedIndexChanged);
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.Location = new System.Drawing.Point(35, 286);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(37, 13);
+            this.linkLabel.TabIndex = 2;
+            this.linkLabel.TabStop = true;
+            this.linkLabel.Text = "GoTo:";
+            this.linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(38, 121);
+            this.descriptionTextBox.Multiline = true;
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.ReadOnly = true;
+            this.descriptionTextBox.Size = new System.Drawing.Size(259, 134);
+            this.descriptionTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "MEV RSS FEED";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(405, 321);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.linkLabel);
+            this.Controls.Add(this.titlesComboBox);
+            this.Controls.Add(this.refreshButton);
+            this.Name = "Form1";
+            this.Text = "RssReader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.ComboBox titlesComboBox;
+        private System.Windows.Forms.LinkLabel linkLabel;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- RSS_reader_MEV/Form1.cs
+++ RSS_reader_MEV/Form1.cs
@@ -7,14 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace RSS_reader_MEV
 {
     public partial class Form1 : Form
     {
+        public List<string> opisi = new List<string>();
+        public List<string> linkovi = new List<string>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            XmlDocument rssXmlDoc = new XmlDocument();
+
+            // Load the RSS file from the RSS URL
+            rssXmlDoc.Load("https://www.mev.hr/index.php/category/racunarstvo-rss/racunarstvo/feed/");
+
+            // Parse the Items in the RSS file
+            XmlNodeList rssNodes = rssXmlDoc.SelectNodes("rss/channel/item");           
+
+            // Iterate through the items in the RSS file
+            foreach (XmlNode rssNode in rssNodes)
+            {                
+                XmlNode rssSubNode = rssNode.SelectSingleNode("title");
+                string title = rssSubNode != null ? rssSubNode.InnerText : "";
+                titlesComboBox.Items.Add(title);
+
+                rssSubNode = rssNode.SelectSingleNode("link");
+                string link = rssSubNode != null ? rssSubNode.InnerText : "";
+                linkovi.Add(link);
+               
+                rssSubNode = rssNode.SelectSingleNode("description");
+                string description = rssSubNode != null ? rssSubNode.InnerText : "";
+                opisi.Add(description);              
+            }
+        }
+
+        private void titlesComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            descriptionTextBox.Text = opisi[titlesComboBox.SelectedIndex];
+            linkLabel.Text = linkovi[titlesComboBox.SelectedIndex];
+            
+        }
+
+        private void linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(linkLabel.Text);
         }
     }
 }
