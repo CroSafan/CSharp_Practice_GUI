7fc66a1cee3f43dda51fa9fe0ef78385702c6f01
Antun Safaric <crosafan@gmail.com>
2016-11-18 19:29:02 +0100

    Checking if site is up or not

--- Site_Checker/Form1.Designer.cs
+++ Site_Checker/Form1.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.checkWebSiteButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // checkWebSiteButton
+            // 
+            this.checkWebSiteButton.Location = new System.Drawing.Point(106, 153);
+            this.checkWebSiteButton.Name = "checkWebSiteButton";
+            this.checkWebSiteButton.Size = new System.Drawing.Size(75, 23);
+            this.checkWebSiteButton.TabIndex = 0;
+            this.checkWebSiteButton.Text = "Check";
+            this.checkWebSiteButton.UseMnemonic = false;
+            this.checkWebSiteButton.UseVisualStyleBackColor = true;
+            this.checkWebSiteButton.Click += new System.EventHandler(this.checkWebSiteButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.checkWebSiteButton);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Site checker";
@@ -43,6 +56,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button checkWebSiteButton;
     }
 }
 
--- Site_Checker/Form1.cs
+++ Site_Checker/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,6 +24,31 @@ namespace Site_Checker
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void checkWebSiteButton_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(@"https://http://zmaj1.com/");
+            request.AllowAutoRedirect = false; // find out if this site is up and don't follow a redirector
+            request.Method = "HEAD";
+            try
+            {
+                WebResponse response = request.GetResponse();
+                if(response.Headers.Count != 0)
+                {
+                    MessageBox.Show("Page active");
+                }else
+                {
+                    MessageBox.Show("Page not active");
+                }
+                
+                // do something with response.Headers to find out information about the request
+            }
+            catch (WebException wex)
+            {
+                MessageBox.Show("Page not active");
+                //set flag if there was a timeout or some other issues
+            }
         }
     }
 }
