45703f6e5c15c0c79740c76b8736d86fe2bacea9
Antun Safaric <crosafan@gmail.com>
2016-11-19 23:42:04 +0100

    Set picture as wallpaper

--- Set_Desktop_Background/Form1.Designer.cs
+++ Set_Desktop_Background/Form1.Designer.cs
@@ -28,12 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.choosePictureButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.setImageButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // choosePictureButton
+            // 
+            this.choosePictureButton.Location = new System.Drawing.Point(13, 13);
+            this.choosePictureButton.Name = "choosePictureButton";
+            this.choosePictureButton.Size = new System.Drawing.Size(75, 23);
+            this.choosePictureButton.TabIndex = 0;
+            this.choosePictureButton.Text = "Choose picture";
+            this.choosePictureButton.UseVisualStyleBackColor = true;
+            this.choosePictureButton.Click += new System.EventHandler(this.choosePictureButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Preview:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(251, 75);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // setImageButton
+            // 
+            this.setImageButton.Location = new System.Drawing.Point(15, 223);
+            this.setImageButton.Name = "setImageButton";
+            this.setImageButton.Size = new System.Drawing.Size(75, 23);
+            this.setImageButton.TabIndex = 3;
+            this.setImageButton.Text = "Set image as background";
+            this.setImageButton.UseVisualStyleBackColor = true;
+            this.setImageButton.Click += new System.EventHandler(this.setImageButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.setImageButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.choosePictureButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button choosePictureButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button setImageButton;
     }
 }
 
--- Set_Desktop_Background/Form1.cs
+++ Set_Desktop_Background/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,44 @@ namespace Set_Desktop_Background
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll")]
+        private static extern bool SystemParametersInfo(uint uiAction, uint uiParam, string pvParam, uint fWinIni);
+        const uint SPI_SETDESKWALLPAPER = 0x14;
+        const uint SPIF_UPDATEINIFILE = 0x01;
+
+
         public Form1()
         {
             InitializeComponent();
+            setImageButton.Hide();
         }
+
+        private void choosePictureButton_Click(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Height = 800;
+            pictureBox1.Width = 800;
+            
+            openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                slika = openFileDialog1.FileName;
+            }
+            setImageButton.Show();
+            setImageButton.Location = new Point(15,900);
+        }
+        string slika = "";
+        private void setImageButton_Click(object sender, EventArgs e)
+        {
+            SetDWallpaper(slika);
+        }
+        public void SetDWallpaper(string path)
+        {
+            //setting the wallpaper to the path
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE);
+        }
+        
     }
 }
