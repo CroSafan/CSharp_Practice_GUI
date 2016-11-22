1bf577bcfdc15b38ddb13fffd3de413089441ad6
Antun Safaric <crosafan@gmail.com>
2016-11-22 21:56:49 +0100

    Added open text file method and reads the text in the file

--- Notepad+++/MainWindow.xaml.cs
+++ Notepad+++/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ namespace Notepad___
     public partial class MainWindow : Window
     {
         public static SaveFileDialog sfd = new SaveFileDialog();
+        public static OpenFileDialog ofd = new OpenFileDialog();
 
         public MainWindow()
         {
@@ -26,6 +27,22 @@ namespace Notepad___
         //open File
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
         {
+            ofd.DefaultExt = ".txt";
+            ofd.Filter = "txt files (*.txt)|*.txt";
+            var result = ofd.ShowDialog();
+            if (result == true)
+            {
+                using (StreamReader sr = new StreamReader(ofd.FileName))
+                {
+                    mainTextBox.Document.Blocks.Clear();
+                    while (sr.EndOfStream ==false)
+                    {
+                        mainTextBox.Document.Blocks.Add(new Paragraph(new Run(sr.ReadLine())));
+                    }
+                }
+            }
+           
+            
         }
 
         //save File
@@ -36,10 +53,10 @@ namespace Notepad___
             var result = sfd.ShowDialog();
             if (result == true)
             {
-                using (StreamWriter sr = new StreamWriter(sfd.FileName))
+                using (StreamWriter sw = new StreamWriter(sfd.FileName))
                 {
                     string textRange = new TextRange(mainTextBox.Document.ContentStart, mainTextBox.Document.ContentEnd).Text;
-                    sr.WriteLine(textRange);
+                    sw.WriteLine(textRange);
                 }
             }
         }
